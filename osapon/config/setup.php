dc75164c810d7258e2c5bfd8c1482b3427479e50
Olga VIRCHENKO <ovirchen@e1r2p18.unit.ua>
2019-09-17 19:58:35 +0300

    rm rf

--- osapon/config/setup.php
+++
@@ -1,4 +0,0 @@
-<?php
-    exec("/Users/ovirchen/MAMP/mysql/bin/mysql -uroot -pqwerty < database.sql  2>&- ");
-    echo "Camagru database was created.\n";
-?>
--- osapon/resources/js/CONST_NAMES.js
+++
@@ -1,6 +0,0 @@
-const HOST = "http://localhost:";
-const PORT = "8080";
-const WEB = "/";
-const API = "/api";
-const WEB_PATH = HOST + PORT + WEB;
-const API_PATH = HOST + PORT + API;
